3a95e72880ad203615effcf9df4f1400b7938e5a
Stein121 <qscaxz0@gmail.com>
2019-09-11 21:48:50 +0800

    Add v6

---
+++ v6/data/index.js
@@ -0,0 +1,100 @@
+const mysql = require('mysql2')
+const uuid = require('uuid/v4')
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: process.env.MysqlUser, // replace with your own user and password
+    password: process.env.MysqlPassword,
+    database: 'se_1',
+})
+
+const dataAccess = {
+    getAllProducts() {
+        return new Promise(function(resolve, reject) {
+            pool.query('SELECT * FROM products', function(err, results) {
+                if (err) {
+                    reject(err)
+                    return
+                }
+
+                resolve(results)
+            })
+        })
+    },
+    getProduct(id) {
+        return new Promise(function(resolve, reject) {
+            pool.query(
+                `SELECT * FROM products
+                WHERE id = ?`,
+                [id],
+                function(err, results) {
+                    // if has error or no product by that `id`
+                    if (err || results.length < 1) {
+                        reject(`No product by id: ${id}`)
+                        return
+                    }
+
+                    resolve(results[0])
+                }
+            )
+        })
+    },
+    addProduct(name, price) {
+        return new Promise(function(resolve, reject) {
+            let productId = uuid()
+            pool.query(
+                `INSERT INTO products
+                VALUES
+                (?, ?, ?)`,
+                [productId, name, price],
+                function(err) {
+                    if (err) {
+                        reject(err)
+                        return
+                    }
+
+                    resolve(productId)
+                }
+            )
+        })
+    },
+    editProduct(id, name, price) {
+        return new Promise(function(resolve, reject) {
+            pool.query(
+                `UPDATE products
+                SET name = ?,
+                price = ?
+                WHERE id = ?`,
+                [name, price, id],
+                function(err) {
+                    if (err) {
+                        reject(err)
+                        return
+                    }
+
+                    resolve(true)
+                }
+            )
+        })
+    },
+    deleteProduct(id) {
+        return new Promise(function(resolve, reject) {
+            pool.query(
+                `DELETE FROM products
+                WHERE id = ?`,
+                [id],
+                function(err) {
+                    if (err) {
+                        reject(err)
+                        return
+                    }
+
+                    resolve(err)
+                }
+            )
+        })
+    },
+}
+
+module.exports = dataAccess
